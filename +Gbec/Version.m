b86e04218d27c35997e7540c9b4c96181eb2b92c
埃博拉酱 <vhtmfu@outlook.com>
2023-02-25 12:44:04 +0800

    修复各种bug

--- +Gbec/Version.m
+++ +Gbec/Version.m
@@ -3,3 +3,7 @@ V.Me='5.1.0';
 V.MatlabExtension=MATLAB.Version;
 V.MATLAB='R2022a';
 V.Deploy=2;
+persistent NewVersion
+if isempty(NewVersion)
+	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/ShanghaitechGuanjisongLab/Generic-Behavioural-Experimental-Control/releases','通用行为实验控制',V.Me);
+end
