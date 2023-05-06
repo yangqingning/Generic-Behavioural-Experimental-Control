fa64004d5b1f926de4f800a744ef03d84abc9e71
埃博拉酱 <vhtmfu@outlook.com>
2023-05-06 15:20:16 +0800

    IHostAction注释

--- +Gbec/IHostAction.m
+++ +Gbec/IHostAction.m
@@ -1,7 +1,9 @@
 classdef IHostAction<handle
 	%设为ExperimentWorker的HostAction所必须实现的接口
 	methods(Abstract)
+		%Arduino发来Signal_HostAction时执行
 		Run(obj,Serial,EventLogger)
+		%会话结束后，将用此方法获取HostAction信息
 		Info=GetInformation(obj)
 	end
 end
