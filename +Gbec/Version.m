8a44ae76738a86ea37fa83b16a8063a3dfcfe51d
埃博拉酱 <vhtmfu@outlook.com>
2023-06-01 10:01:45 +0800

    在事件记录中增加回合开始事件，以便后期分割回合

--- +Gbec/Version.m
+++ +Gbec/Version.m
@@ -2,7 +2,7 @@ function V=Version
 V.Me='v5.2.1';
 V.MatlabExtension=MATLAB.Version;
 V.MATLAB='R2022b';
-V.Deploy=3;
+V.Deploy=4;
 persistent NewVersion
 if isempty(NewVersion)
 	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/ShanghaitechGuanjisongLab/Generic-Behavioural-Experimental-Control/releases','通用行为实验控制',V.Me);
