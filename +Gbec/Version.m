4d7b81917088a758a592cf7923c57fb805b42023
埃博拉酱 <vhtmfu@outlook.com>
2023-09-04 14:42:47 +0800

    部署版本更新

--- +Gbec/Version.m
+++ +Gbec/Version.m
@@ -2,7 +2,7 @@ function V=Version
 V.Me='v5.3.0';
 V.MatlabExtension=MATLAB.Version;
 V.MATLAB=matlabRelease().Release;
-V.Deploy=4;
+V.Deploy=5;
 persistent NewVersion
 if isempty(NewVersion)
 	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/ShanghaitechGuanjisongLab/Generic-Behavioural-Experimental-Control/releases','通用行为实验控制',V.Me);
