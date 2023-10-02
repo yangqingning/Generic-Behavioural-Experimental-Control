1f48b925ad3fea64ef4c4cedc30d373a51a9ce0e
埃博拉酱 <vhtmfu@outlook.com>
2023-10-02 14:07:31 +0800

    看门狗默认时间1分钟

--- +Gbec/@ExperimentWorker/ExperimentWorker.m
+++ +Gbec/@ExperimentWorker/ExperimentWorker.m
@@ -159,7 +159,7 @@ classdef ExperimentWorker<handle
 		function obj=ExperimentWorker
 			%构造对象，建议使用MATLAB.Lang.Owner包装对象，不要直接存入工作区，否则清空变量时可能不能正确断开串口
 			disp(['通用行为实验控制器' Gbec.Version().Me ' by 张天夫']);
-			obj.WatchDog=timer(StartDelay=30,TimerFcn=@(~,~)ReleaseSerial(obj.Serial));
+			obj.WatchDog=timer(StartDelay=60,TimerFcn=@(~,~)ReleaseSerial(obj.Serial));
 			obj.EventRecorder=MATLAB.DataTypes.EventLogger;
 			obj.TrialRecorder=MATLAB.DataTypes.EventLogger;
 			obj.PreciseRecorder=MATLAB.Containers.Vector;
